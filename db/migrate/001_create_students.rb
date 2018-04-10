8fb4be07540b5c8c86f1186f084af6bb44c951c4
Morgan Fudge <mfudge92@gmail.com>
2018-04-10 14:57:31 +0000

    automatically backed up by learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,6 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.string :test
       t.boolean :active, default: 'false'
 
       t.timestamps null: false
